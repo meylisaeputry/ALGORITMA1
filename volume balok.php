59819be3921e212daf1fa84098a921290e790422
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-12 19:34:15 +0700

    first commit

---
+++ volume balok.php
@@ -0,0 +1,20 @@
+<?php
+//Mulai
+//Deklarasi,panjang,lebar,dan tinggi
+$panjang =5;
+$lebar =4;
+$lebar =3;
+//Tentukan nilai panjang
+//Tentukan nilai lebar
+//Tentukan nilai tinggi
+echo "panjang=". $panjang;
+echo "<br>lebar=". $lebar;
+echo "<br>tinggi=". $tinggi;
+//Hitung volume dengan panjang dikali lebar dikali tinggi
+$volume=$panjang*$lebar*$lebar;
+//Tampilkan hasil perkalian
+echo "<br>hasil volume balok adalah=". $volume;
+//Selesai
+
+
+Echo $volume;
